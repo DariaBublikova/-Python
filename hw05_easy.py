4d372101a4300f36f54515c0a8552b7db634f97f
Daria Bublikova <dashabublikova46@gmail.com>
2019-08-27 20:25:55 +0300

    уровень easy

---
+++ hw05_easy.py
@@ -0,0 +1,37 @@
+
+import os
+import shutil
+
+# Задача-1:
+# Напишите скрипт, создающий директории dir_1 - dir_9 в папке,
+# из которой запущен данный скрипт.
+# И второй скрипт, удаляющий эти папки.
+
+def make_dir(name):
+    try:
+        os.mkdir(name)
+    except FileExistsError:
+        print('{} уже существует'.format(name))
+
+def remove_dir(name):
+    try:
+        os.rmdir(name)
+    except FileNotFoundError:
+        print('{}  не существует'.format(name))
+
+name = [('dir_' + str(i + 1)) for  i in range(9)]
+
+# Задача-2:
+# Напишите скрипт, отображающий папки текущей директории.
+
+def list_dir(main_path):
+    for _ in os.listdir(main_path):
+        print(_)
+
+main_path = os.getcwd()
+
+# Задача-3:
+# Напишите скрипт, создающий копию файла, из которого запущен данный скрипт.
+
+def copy_file(first_file, copy_file):
+    shutil.copy(first_file, copy_file)
